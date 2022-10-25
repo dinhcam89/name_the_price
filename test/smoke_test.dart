9025fbd9bfae9a7c7b36c9f2fa953c6954c998e0
dcamm79 <dinhcam1512@gmail.com>
2022-10-26 01:02:26 +0700

    update smoke test

---
+++ test/smoke_test.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:name_the_price/main.dart';
+
+void main() {
+  testWidgets('smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+  });
+}
